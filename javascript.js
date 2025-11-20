919cc3a19d1d92848e55b530ab34464c2c2910f3
Joao Oliveira <151586645+JoaoOliveira-web@users.noreply.github.com>
2025-11-20 18:15:40 +0000

    added new fucntions to the buttons

--- javascript.js
+++ javascript.js
@@ -1,29 +1,58 @@
+
+const rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
 const container = document.querySelector(".container");
-const grid_btn = document.querySelector("#grid_btn");
-
-
-function makeNewGrid(){
-    let size = prompt("Enter a grid size from 1-100");
-    makeGrid(size);
-}
+var size = 16;
+var mouse = "default";
 
 function makeGrid(size) {
-    for (i = 0; i < size * size; i++) {
+    container.innerHTML = "";
+
+    for (let i = 0; i < size * size; i++) {
             const cell = document.createElement("div");
             cell.className+="cell";
             container.appendChild(cell);
         }
+
+    var cells = document.getElementsByClassName("cell");
+
+    for(let i = 0 ; i < cells.length; i++){
+        cells[i].onmouseenter = function () { 
+            changeColor(this);
+            this.onmouseenter = null;
+         }
+    }
 }
+
+makeGrid(size);
+
+let colorIndex = 0;
 
 function changeColor(cell){
-    cell.style.backgroundColor = "black";
+    if (mouse === "default")
+        cell.style.backgroundColor = "black";
+    else
+        cell.style.backgroundColor = rainbow[colorIndex];
+        colorIndex = (colorIndex + 1) % 7;
 }
 
+const grid_btn = document.getElementById("grid_btn");
 
-var cells = document.getElementsByClassName("cell");
+grid_btn.addEventListener("click",function(){
+    do{
+          size = prompt("Enter a grid size from 1-100").trim();
+    } while(isNaN(size) || size < 1 || size > 100);
+    document.documentElement.style.setProperty("--size", size);
+    makeGrid(size);
+});
 
-for( i = 0 ; i < cells.length; i++){
-    cells[i].onmouseenter = function () { changeColor(this) }
-}
+const rbg_brush = document.getElementById("rbg_brush")
 
-//grid_btn.addEventListener("click",makeNewGrid());
+rbg_brush.addEventListener("click",function(){
+    mouse = "rbg";
+})
+
+const default_brush = document.getElementById("default_brush")
+
+default_brush.addEventListener("click",function(){
+    mouse = "default";
+})
